2032a812ea7124569ee856c707f559d5a5e9fd2a
KhaiPhamdevbase <khai.pham@devbase.us>
2023-11-25 11:35:33 +0700

    feat: add get weather by city name

--- main.go
+++ main.go
@@ -1,27 +1,38 @@
 package main
 
-// func main() {
-// 	reader := getOsReader()
-// 	loopFreeBirdTool(reader)
-// }
-
 import (
 	"flag"
+	"fmt"
 	"log"
 )
 
 func main() {
-
 	// Define command-line flags
 	city := flag.String("city", "Ho Chi Minh City", "City naming")
 
 	// Parse the command-line flags
 	flag.Parse()
 
-	err := loadEnvFile(".env")
-	if err != nil {
-		log.Fatal(err)
+	// Load environment file before creating new WeatherService
+	if err := loadEnvFile(".env"); err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
 	}
 
-	getWeather(*city) // Handle the flag values
+	service := NewWeatherService()
+
+	// Get coordinates for the provided city
+	coordinates, err := service.getCoordinates(*city)
+	if err != nil {
+		log.Fatalf("Failed to get coordinates: %v", err)
+	}
+
+	// Retrieve weather information for all coordinates
+	for _, coordinate := range coordinates {
+		weather, err := service.getWeather(coordinate.Lat, coordinate.Lon)
+		if err != nil {
+			log.Printf("Error retrieving weather for [%s]: %v\n", coordinate.Name, err)
+			continue
+		}
+		fmt.Printf("Weather in %s: %+v\n", coordinate.Name, weather.Main)
+	}
 }
--- services.go
+++ services.go
@@ -3,10 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"net/url"
 )
 
 type CoordinatesResponse struct {
@@ -15,63 +12,73 @@ type CoordinatesResponse struct {
 	Lon  float64 `json:"lon"`
 }
 
-func getWeather(cityName string) {
-
-	coordinates, err := getCoordinates(cityName)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-
-	fmt.Println(coordinates)
+type Weather struct {
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+	Id          int    `json:"id"`
+	Main        string `json:"main"`
 }
 
-func getCoordinates(cityName string) ([]CoordinatesResponse, error) {
-	apiKey := getEnvOrDefault("API_KEY", "")
-	endpointV1 := getEnvOrDefault("API_GEO_V1", "")
+type Main struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+	TempMin   float64 `json:"temp_min"`
+	TempMax   float64 `json:"temp_max"`
+	Pressure  float64 `json:"pressure"`
+	Humidity  float64 `json:"humidity"`
+	SeaLevel  float64 `json:"sea_level"`
+	GrndLevel float64 `json:"grnd_level"`
+}
 
-	geoResponse, err := makeGetRequest(endpointV1, map[string]string{
-		"q":     cityName,
-		"appid": apiKey,
-	})
+type WeatherResponse struct {
+	Weather []Weather `json:"weather"`
+	Main    Main      `json:"main"`
+}
 
+type WeatherService struct {
+	API_KEY                string
+	API_GEO_V1             string
+	API_CURRENT_WEATHER_V2 string
+	client                 *http.Client
+}
+
+func NewWeatherService() *WeatherService {
+	return &WeatherService{
+		API_KEY:                getEnvOrDefault("API_KEY", ""),
+		API_GEO_V1:             getEnvOrDefault("API_GEO_V1", ""),
+		API_CURRENT_WEATHER_V2: getEnvOrDefault("API_CURRENT_WEATHER_V2", ""),
+		client:                 &http.Client{},
+	}
+}
+
+func (ws *WeatherService) getWeather(lat float64, lon float64) (*WeatherResponse, error) {
+	params := "?lat=" + fmt.Sprintf("%.4f", lat) + "&lon=" + fmt.Sprintf("%.4f", lon) + "&appid=" + ws.API_KEY
+	resp, err := ws.client.Get(ws.API_CURRENT_WEATHER_V2 + params)
 	if err != nil {
 		return nil, fmt.Errorf("error while making the request: %w", err)
 	}
+	defer resp.Body.Close()
 
-	defer closeResponseBody(geoResponse)
-
-	body, err := io.ReadAll(geoResponse.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error while reading the response: %w", err)
+	var weather WeatherResponse
+	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
+		return nil, fmt.Errorf("error unmarshaling response body: %w", err)
 	}
 
+	return &weather, nil
+}
+
+func (ws *WeatherService) getCoordinates(cityName string) ([]CoordinatesResponse, error) {
+	params := "?q=" + cityName + "&appid=" + ws.API_KEY
+	resp, err := ws.client.Get(ws.API_GEO_V1 + params)
+	if err != nil {
+		return nil, fmt.Errorf("error while making the request: %w", err)
+	}
+	defer resp.Body.Close()
+
 	var coordinates []CoordinatesResponse
-	if err := json.Unmarshal(body, &coordinates); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&coordinates); err != nil {
 		return nil, fmt.Errorf("error unmarshaling response body: %w", err)
 	}
 
 	return coordinates, nil
-}
-
-func makeGetRequest(url string, queryParams map[string]string) (*http.Response, error) {
-	query := url + "?" + encodeQueryParams(queryParams)
-	response, err := http.Get(query)
-	if err != nil {
-		return nil, err
-	}
-	return response, nil
-}
-
-func encodeQueryParams(queryParams map[string]string) string {
-	values := url.Values{}
-	for key, value := range queryParams {
-		values.Set(key, value)
-	}
-	return values.Encode()
-}
-
-func closeResponseBody(response *http.Response) {
-	if err := response.Body.Close(); err != nil {
-		log.Printf("Error while closing response body: %v", err)
-	}
 }
